3c69fdd0cc23473382e56fc4da78a651b101a171
freddyshim <shimfreddy@gmail.com>
2022-05-06 00:53:56 -0400

    changed domain name

--- rpistream/app/src/main/java/com/anookday/rpistream/util/Constants.kt
+++ rpistream/app/src/main/java/com/anookday/rpistream/util/Constants.kt
@@ -3,7 +3,7 @@ package com.anookday.rpistream.util
 class Constants {
     companion object {
         const val KILOBYTE_SIZE = 1024
-        const val PP_HOST = "https://anookday.dev"
+        const val PP_HOST = "https://pp.freddyshim.com"
         const val PP_AUTH = "/auth/twitch"
     }
 }
